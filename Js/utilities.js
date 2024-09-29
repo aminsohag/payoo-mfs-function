0f2642a038f116455cf2fae2d00c7e1c4cbc43b9
aminsohag <aminsohag508@gmail.com>
2024-09-29 20:46:16 +0600

    first commit

---
+++ Js/utilities.js
@@ -0,0 +1,23 @@
+/**
+ * Common Shared functions here
+ */
+
+// function getInputFieldValueById(){
+//     console.log('function by clicked');   
+// }
+
+// console.log('utilities loaded');
+
+
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+    
+// }
+
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
